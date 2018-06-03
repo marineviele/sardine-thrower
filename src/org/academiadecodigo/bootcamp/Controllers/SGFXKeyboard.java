74b760806717a8e820cb20c11b348f5233302dca
Nuno Ramalho <nuno.miguel.ramalho@gmail.com>
2018-06-03 13:47:31 +0100

    fix: keyboard

--- src/org/academiadecodigo/bootcamp/Controllers/SGFXKeyboard.java
+++ src/org/academiadecodigo/bootcamp/Controllers/SGFXKeyboard.java
@@ -7,47 +7,42 @@ import org.academiadecodigo.simplegraphics.keyboard.KeyboardEvent;
 import org.academiadecodigo.simplegraphics.keyboard.KeyboardEventType;
 import org.academiadecodigo.simplegraphics.keyboard.KeyboardHandler;
 
-/**
- * Created by codecadet on 03/06/2018.
- */
-public class SGFXKeyboard {
-    public class SimpleGFXKeyboard implements KeyboardHandler {
+public class SGFXKeyboard implements KeyboardHandler {
 
-        Player player;
+    Player player;
 
-        public void initKeyboard(Player player){
-            this.player = player;
-            Keyboard keyboard = new Keyboard(this);
+    public void initKeyboard(Player player) {
+        this.player = player;
+        Keyboard keyboard = new Keyboard(this);
 
-            KeyboardEvent left = new KeyboardEvent();
-            left.setKey(KeyboardEvent.KEY_LEFT);
-            left.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+        KeyboardEvent left = new KeyboardEvent();
+        left.setKey(KeyboardEvent.KEY_LEFT);
+        left.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
 
-            KeyboardEvent right = new KeyboardEvent();
-            right.setKey(KeyboardEvent.KEY_RIGHT);
-            right.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+        KeyboardEvent right = new KeyboardEvent();
+        right.setKey(KeyboardEvent.KEY_RIGHT);
+        right.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
 
-            keyboard.addEventListener(left);
-            keyboard.addEventListener(right);
+        keyboard.addEventListener(left);
+        keyboard.addEventListener(right);
+    }
+
+    @Override
+    public void keyPressed(KeyboardEvent keyboardEvent) {
+
+        switch (keyboardEvent.getKey()) {
+            case KeyboardEvent.KEY_LEFT:
+                player.move(Direction.LEFT);
+                break;
+            case KeyboardEvent.KEY_RIGHT:
+                player.move(Direction.RIGHT);
+                break;
         }
 
-        @Override
-        public void keyPressed(KeyboardEvent keyboardEvent) {
+    }
 
-            switch (keyboardEvent.getKey()){
-                case KeyboardEvent.KEY_LEFT:
-                    player.move(Direction.LEFT);
-                    break;
-                case KeyboardEvent.KEY_RIGHT:
-                    player.move(Direction.RIGHT);
-                    break;
-            }
+    @Override
+    public void keyReleased(KeyboardEvent keyboardEvent) {
 
-        }
-
-        @Override
-        public void keyReleased(KeyboardEvent keyboardEvent) {
-
-        }
     }
 }
