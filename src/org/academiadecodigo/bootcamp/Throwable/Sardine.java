d7c421768a8a070719285a57ce6c7e47607b4a2e
Nuno Ramalho <nuno.miguel.ramalho@gmail.com>
2018-06-03 14:12:50 +0100

    fix: conflicts

--- src/org/academiadecodigo/bootcamp/Throwable/Sardine.java
+++ src/org/academiadecodigo/bootcamp/Throwable/Sardine.java
@@ -3,13 +3,9 @@ package org.academiadecodigo.bootcamp.Throwable;
 import org.academiadecodigo.bootcamp.Position.Position;
 
 public class Sardine extends AbstractThrowable {
-
-
     public Sardine (Position position) {
-
         super.setPosition(position);
     }
-
 
     //draw
 }
