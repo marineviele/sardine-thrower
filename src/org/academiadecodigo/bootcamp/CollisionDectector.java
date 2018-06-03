222e95c06546151e59cff2dbf2c46cfd9015b76b
Marine <marinehenriques@gmail.com>
2018-06-03 15:05:31 +0100

    fix: collision detector

--- src/org/academiadecodigo/bootcamp/CollisionDectector.java
+++ src/org/academiadecodigo/bootcamp/CollisionDectector.java
@@ -7,23 +7,11 @@ import org.academiadecodigo.bootcamp.Throwable.Throwable;
 
 public class CollisionDectector {
 
-    public int larguraPlayer = 20;
-    public int alturaPlayer = 10;
-    public int larguraThrowable = 10;
-    public int alturaThrowable = 5;
-    private Player player;
-    int playerPosY = 10;
-
-
-    public CollisionDectector(int larguraPlayer, int alturaPlayer, int larguraThrowable, int alturaThrowable) {
-
-    }
     public CollisionDectector() {
 
     }
 
     public boolean collisionChecker(Player player, Throwable throwable, int y) {
-        player = this.player;
 
         if ((throwable.getPosition().getY() == player.getPosition().getY() &&
                 throwable.getPosition().getX() == player.getPosition().getX())
